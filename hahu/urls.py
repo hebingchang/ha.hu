25b8c97de8ff0e7d8bee92afba9cbe5c6eed4909
Songzhou Yang <yangsongzhou@outlook.com>
2016-07-22 02:28:33 +0800

    fix urls bug

--- hahu/urls.py
+++ hahu/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', 'polls.views.login')
+    url(r'^admin/', admin.site.urls)
 ]
