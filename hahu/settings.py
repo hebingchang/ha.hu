85ec36ca3695bcf5de6a4c78728734aba26827f8
TennnyZhuang <zty0826@gmail.com>
2016-07-21 11:19:12 +0800

    django-extensions: graph models

--- hahu/settings.py
+++ hahu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPH_MODELS = {
+  'group_models': True,
+}
