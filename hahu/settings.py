fb6b83f0e4695893549032a5e3317d9ab20b55cd
TennnyZhuang <zty0826@gmail.com>
2016-07-22 01:17:44 +0800

    login page

--- hahu/settings.py
+++ hahu/settings.py
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join('static'),)
 
 GRAPH_MODELS = {
     'group_models': True,
