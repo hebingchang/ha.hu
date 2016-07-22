bd0cb25e2fe2f45fdc8db78380e76d2ec63bd220
Songzhou Yang <yangsongzhou@outlook.com>
2016-07-22 17:28:19 +0800

    modify signup

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,3 +1,11 @@
-/**
- * Created by yangs on 7/22/2016.
- */
+$(function () {
+  $(".mdl-textfield__input").blur(function () {
+    $(this).prop('required', true);
+    $(this).parent().addClass('is-invalid');
+  });
+
+  $(".mdl-button[type='submit']").click(function () {
+    $(this).parent().siblings().children(".mdl-textfield").addClass('is-invalid');
+    $(this).parent().siblings().children(".mdl-textfield").children(".mdl-textfield__input").prop('required', true);
+  });
+});
