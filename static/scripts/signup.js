964d4b2745f4515d50b38c58f77a62af9937bb9b
Songzhou Yang <yangsongzhou@outlook.com>
2016-07-22 17:48:23 +0800

    modify input judge

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,11 +1,9 @@
 $(function () {
   $(".mdl-textfield__input").blur(function () {
     $(this).prop('required', true);
-    $(this).parent().addClass('is-invalid');
   });
 
   $(".mdl-button[type='submit']").click(function () {
-    $(this).parent().siblings().children(".mdl-textfield").addClass('is-invalid');
     $(this).parent().siblings().children(".mdl-textfield").children(".mdl-textfield__input").prop('required', true);
   });
 });
