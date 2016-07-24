4272823bce5330f9148edc5a4fdcdb0e2fff261b
TennnyZhuang <zty0826@gmail.com>
2016-07-25 07:54:26 +0800

    celery

--- hahu/settings.py
+++ hahu/settings.py
@@ -124,3 +124,11 @@ STATICFILES_DIRS = (os.path.join('static'),)
 GRAPH_MODELS = {
     'group_models': True,
 }
+
+# celery
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
