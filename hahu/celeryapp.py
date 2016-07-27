c4ccc821cb91bc457fe30f337a7d86c9a10f4e25
TennnyZhuang <zty0826@gmail.com>
2016-07-27 17:56:55 +0800

    todo: shared task

--- hahu/celeryapp.py
+++ hahu/celeryapp.py
@@ -15,3 +15,6 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, related_name='tasks')
 
 from polls.tasks import new_feed
+from polls.tasks import delete_feed
+from polls.tasks import new_follow
+from polls.tasks import delete_follow
--- polls/tasks.py
+++ polls/tasks.py
@@ -8,3 +8,18 @@ from . import cache
 def new_feed(user_id, follower_names, feed, feed_id):
     cache.new_feed(user_id=user_id, feed_id=feed_id, feed=feed)
     cache.update_feeds_set(user_ids=follower_names, feed_ids=[feed_id])
+
+
+@shared_task
+def delete_feed(user_id, followers_names, feed_id):
+    raise NotImplementedError
+
+
+@shared_task
+def new_follow(from_user_id, to_user_id, feeds):
+    raise NotImplementedError
+
+
+@shared_task
+def delete_follow():
+    raise NotImplementedError
