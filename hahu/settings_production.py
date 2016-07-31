86ae4f20768184b8f8d671ada17d089ed9c43555
TennnyZhuang <zty0826@gmail.com>
2016-07-31 17:35:12 +0800

    db init command

--- hahu/settings_production.py
+++ hahu/settings_production.py
@@ -4,17 +4,20 @@ DATABASES = {
         'NAME': 'hahu',
         'HOST': 'database',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET foreign_key_checks = 0;',
+        },
     }
 }
 
 REDIS_HOST = 'redis'
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
